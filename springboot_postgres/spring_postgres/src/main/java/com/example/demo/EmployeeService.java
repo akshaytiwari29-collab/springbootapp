2ee9b8fd66ba5af0a323b9b8b4188e3cb96e2d26
akshaytiwari29-collab <81382911+akshaytiwari29-collab@users.noreply.github.com>
2021-03-25 23:31:50 +0530

    Add files via upload

---
+++ springboot_postgres/spring_postgres/src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+	List<Employee> findAll();
+
+	void insertEmployee(Employee emp);
+
+	void updateEmployee(Employee emp);
+
+	void executeUpdateEmployee(Employee emp);
+
+	void deleteEmployee(Employee emp);
+	
+}
